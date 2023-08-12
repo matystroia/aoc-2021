3bcdcac15b23fa0d2e99ac9583cf164e0c9d77ff
Matei <stroiamatei@gmail.com>
2023-08-12 14:25:32 +0300

    add nav item for home

--- app/day/Navigation.js
+++ app/day/Navigation.js
@@ -4,38 +4,45 @@ import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { ChallengeNames } from "../input/names";
+import { HomeIcon } from "@heroicons/react/24/solid";
 
-function DayLink({ day }) {
-    const pathname = usePathname();
-    const href = `/day/${day}`;
-    const isActive = pathname === href;
-    const name = ChallengeNames[day];
-
+function NavLink({ icon, title, href, isActive }) {
     return (
         <Link
-            key={day}
             href={href}
             className={clsx(
                 isActive && "bg-slate-500",
                 "w-full h-10 py-1 flex items-center border-b border-slate-500 hover:bg-slate-500 relative"
             )}
         >
-            <div className="w-10 font-mono text-center shrink-0">{day}</div>
+            <div className="w-10 font-mono text-center shrink-0">{icon}</div>
             <div className="overflow-hidden text-xs text-center transition-opacity opacity-0 text-slate-300 text-ellipsis whitespace-nowrap group-hover:opacity-100">
-                {name}
+                {title}
             </div>
         </Link>
     );
 }
 
 export function Navigation() {
+    const pathname = usePathname();
     const days = Array.from({ length: 25 }, (_, i) => i + 1);
 
     return (
         <nav className="flex flex-col w-10 overflow-x-hidden overflow-y-auto bg-slate-700 hover:w-64 transition-[width] group">
-            {days.map((day) => (
-                <DayLink key={day} day={day} />
-            ))}
+            <NavLink
+                icon={<HomeIcon className="inline-block w-5 h-5" />}
+                title="Home"
+                href="/"
+                isActive={false}
+            />
+            {days.map((day) => {
+                const href = `/day/${day}`;
+                const isActive = pathname === href;
+                const name = ChallengeNames[day];
+                return (
+                    <NavLink key={day} icon={day} title={name} href={href} isActive={isActive} />
+                );
+            })}
             {/* <div className="mt-auto h-14 bg-emerald-500"></div> */}
         </nav>
     );
